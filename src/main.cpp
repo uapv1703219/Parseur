fb52a2e01c8f43da8b9054a52c7b6ed299bc8f1f
Gerard Florian <florian.gerard2@alumni.univ-avignon.fr>
2018-12-21 11:01:52 +0100

    Premier test

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	system("pdf2txt -o ../CONVERT/test ../Papers/Alexandrov_2015_A\\ Modified\\ Tripartite\\ Model\\ for\\ Document\\ Representation\\ in\\ Internet\\ Sociology.pdf");
+	return 0;
+}
